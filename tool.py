49f249486ab0515605264a667f364c94539aaabe
Bhavika Bhalani <bbhalani@albany.edu>
2017-10-27 20:45:39 -0400

    review1_tool
    tool.py modified after 1st review

--- tool.py
+++ tool.py
@@ -1,52 +1,65 @@
-#!/usr/bin/env python 2.7
+#!/usr/bin/env python2.7
 import psycopg2
 from datetime import datetime
 
 
-# fuction to get the most popular three articles of all time
+def conn(query):
+    try:
+        db = psycopg2.connect(database="news")
+        c = db.cursor()
+        c.execute(query)
+        results = c.fetchall()
+        db.close()
+        return results
+    except Exception as e:
+        raise e
+
+
 def articles():
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    query1 = """select a.title,v.views from articles as a join
-    artbyviews as v on a.slug = v.slug order by v.views desc limit 3;"""
-    c.execute(query1)
-    results = c.fetchall()
+    """ fuction to get the most popular three articles of all time"""
+    query = """SELECT a.title, v.views
+                FROM articles AS a
+                JOIN artbyviews AS v
+                ON a.slug = v.slug
+                ORDER BY v.views DESC
+                LIMIT 3;"""
+    results = conn(query)
     print("The most popular three articles of all time:")
     for result in results:
         print("\t\"" + str(result[0])+"\" - " + str(result[1]) + " views")
-    db.close()
 
 
-# fuction to get the most popular authors of all time
 def authors():
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    query2 = """select au.name, sum(v.views) as views from artbyviews as v inner
-                join articles as a on v.slug = a.slug inner join authors as au
-                on a.author = au.id group by au.name order by views desc;"""
-    c.execute(query2)
-    results = c.fetchall()
+    """ fuction to get the most popular authors of all time."""
+    query = """SELECT au.name, sum(v.views) AS views
+                FROM artbyviews AS v
+                INNER JOIN articles AS a
+                ON v.slug = a.slug
+                INNER JOIN authors AS au
+                ON a.author = au.id
+                GROUP BY au.name
+                ORDER BY views DESC;"""
+    results = conn(query)
     print("The most popular article authors of all time:")
     for result in results:
         print("\t"+str(result[0]) + " - " + str(result[1]) + " views")
-    db.close()
 
 
-# function to get the day on which more than 1% of requests led to errors
 def errors():
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    query3 = """select * from (select e.day, round((e.error*100)/cast(t.total
-                as decimal(10,2)),2) as errors from totalview as t join
-                errorview as e on t.day=e.day) as t where errors>1.0;"""
-    c.execute(query3)
-    results = c.fetchall()
+    """ function to get the day on which more than 1% of requests led to errors
+    """
+    query = """SELECT * FROM (
+                SELECT e.day,
+                ROUND((e.error*100)/CAST(t.total AS DECIMAL(10,2)),2) AS errors
+                FROM totalview AS t
+                JOIN errorview AS e ON t.day=e.day) AS t
+                WHERE errors>1.0;"""
+    results = conn(query)
     print("The day on which more than 1% of requests led to errors:")
-    for result in results:
-        print("\t"+str(result[0].strftime("%b %d, %y")) + " - " +
-              str(result[1]) + "% errors")
-    db.close()
+    for day, errors in results:
+        print("\t{0:%B %d, %Y} - {1}% errors".format(day, errors))
 
-articles()
-authors()
-errors()
+if __name__ == '__main__':
+    articles()
+    authors()
+    errors()
